daa3b1fcfdfc79cb076a95b9c9d7ab1446c3b2cf
obrianmoreira <brian.agnilo@gmail.com>
2024-01-17 22:19:20 +0000

    added middleware

---
+++ middleware.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+
+export async function middleware(request) {
+    const origin = request.headers.get('origin');
+    console.log(origin);
+
+    const response = NextResponse.next();
+    response.headers.set('Acess-Control-Allow-Origin', /^https:\/\/(www.\.)?(yourdomain\.com)$/i.test(origin) ? origin : '');
+    response.headers.set('Acess-Control-Allow-Methods', 'GET, POST, PUST, DELETE, OPTIONS');
+    response.headers.set('Acess-Control-Allow-Headers', 'Content-Type, Authorization');
+    response.headers.set('Acess-Control-Max-Age', '86400');
+
+    console.log('Middleware');
+    console.log(request.method);
+    console.log(request.url);
+
+    return response;
+    
+}
+
+export const config = {
+    matcher: '/api/:path*'
+};
