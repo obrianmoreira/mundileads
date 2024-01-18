73e16318e241186c8f30d5db8d15ced0fbf60cbd
obrianmoreira <110128226+obrianmoreira@users.noreply.github.com>
2024-01-18 22:23:08 +0000

    Update route.jsx

--- app/api/mundi/route.jsx
+++ app/api/mundi/route.jsx
@@ -1,3 +1,4 @@
+import { Response } from 'next';
 import { CreateMundiLead } from "@/components/crud/create";
 import { ReadMundiLead } from "@/components/crud/read";
 
@@ -12,7 +13,12 @@ export async function POST(request) {
     return Response.json('POST DEU CERTO, CARALHO!')
 }
 
-export async function GET(){
-    const mundiLead = await ReadMundiLead();
-    return Response.json(mundiLead);
+export async function GET(request) {
+  const origin = request.headers.get('origin');
+  const mundiLead = await ReadMundiLead();
+  const headers = {
+    'Access-Control-Allow-Origin': origin || '*',
+    'Content-type': 'application/json',
+  };
+  return new Response(JSON.stringify(mundiLead), { headers });
 }
