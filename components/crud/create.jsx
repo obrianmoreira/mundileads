0d1e7f3b7d94613e3b236f10cde0a4b57bbd4866
obrianmoreira <110128226+obrianmoreira@users.noreply.github.com>
2024-01-19 17:26:56 +0000

    Update create.jsx

--- components/crud/create.jsx
+++ components/crud/create.jsx
@@ -3,14 +3,14 @@ import { database } from "./database";
 
 export async function CreateMundiLead(name, userName, phone, birth) {
     try {
-        const docRef = collection(database, 'MundiLead').doc();
+        const docRef = collection(database, 'MundiLead');
         await setDoc(doc(docRef), {
             name : name,
             userName: userName,
             phone: phone,
             birth: birth,
-        })
+        });
     } catch(error) {
-        console.log('errou, hein!')
+        console.log('errou, hein!');
     }
 };
