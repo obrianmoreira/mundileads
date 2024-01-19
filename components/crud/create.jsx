e298fb12a7e8675019d3eb4882de314be229d2be
obrianmoreira <110128226+obrianmoreira@users.noreply.github.com>
2024-01-19 17:23:33 +0000

    Update create.jsx

--- components/crud/create.jsx
+++ components/crud/create.jsx
@@ -3,7 +3,7 @@ import { database } from "./database";
 
 export async function CreateMundiLead(name, userName, phone, birth) {
     try {
-        const docRef = collection(database, 'MundiLead');
+        const docRef = collection(database, 'MundiLead').doc();
         await setDoc(doc(docRef), {
             name : name,
             userName: userName,
