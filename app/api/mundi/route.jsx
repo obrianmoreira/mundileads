92d8919e754c236ee4b88d9c08d138dd437e2c95
obrianmoreira <110128226+obrianmoreira@users.noreply.github.com>
2024-01-20 20:42:02 +0000

    Update route.jsx

--- app/api/mundi/route.jsx
+++ app/api/mundi/route.jsx
@@ -11,42 +11,17 @@ export async function POST(request) {
         const phone = leads.phone;
         const birth = leads.birth;
         CreateMundiLead(name, userName, phone, birth);
-        return Response.json(JSON.stringify({message: 'POST DEU CERTO, CARALHO'}),
-        {
-            headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*',
-            },
-        });
+        return Response.json('POST DEU CERTO, CARALHO');
     } catch (error) {
-        return Response.json(JSON.stringify({error: 'ALGO DEU ERRADO'}),
-        {
-            headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*',
-            },
-            status: 500,
-        });
+        return Response.json({error: 'ALGO DEU ERRADO'});
     }
 }
 
 export async function GET(){
     try {
         const mundiLead = await ReadMundiLead();
-        return Response.json({message: mundiLead}, {
-            headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*',
-            },
-            status: 200,
-        });
+        return Response.json(mundiLead);
     } catch(error){
-        return Response.json({error: "SOMETHING WENT WRONG"}, {
-            headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*',
-            },
-            status: 500,
-        });
+        return Response.json({error: "SOMETHING WENT WRONG"});
     }
 }
