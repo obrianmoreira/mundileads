e7d6253eefa4a6d203fe540fb4170c5ef3e8d1c3
obrianmoreira <110128226+obrianmoreira@users.noreply.github.com>
2024-01-20 17:47:41 +0000

    Update route.jsx

--- app/api/mundi/route.jsx
+++ app/api/mundi/route.jsx
@@ -1,19 +1,52 @@
 import { CreateMundiLead } from "@/components/crud/create";
 import { ReadMundiLead } from "@/components/crud/read";
-import { NextResponse } from "next/server";
+
 
 export async function POST(request) {
-    const data = await request.json();
-    const {leads} = data;
-    const name = leads.name;
-    const userName = leads.userName;
-    const phone = leads.phone;
-    const birth = leads.birth;
-    CreateMundiLead(name, userName, phone, birth);
-    return NextResponse.json('POST DEU CERTO, CARALHO!')
+    try {
+        const data = await request.json();
+        const {leads} = data;
+        const name = leads.name;
+        const userName = leads.userName;
+        const phone = leads.phone;
+        const birth = leads.birth;
+        CreateMundiLead(name, userName, phone, birth);
+        return Response.json(JSON.stringify({message: 'POST DEU CERTO, CARALHO'}),
+        {
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+            },
+        });
+    } catch (error) {
+        return Response.json(JSON.stringify({error: 'ALGO DEU ERRADO'}),
+        {
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+            },
+            status: 500,
+        });
+    }
 }
 
-export async function GET(request) {
-  const mundiLead = await ReadMundiLead();    
-  return NextResponse.json(mundiLead);
+export async function GET(){
+    try {
+        const mundiLead = await ReadMundiLead();
+        return Response.json({message: mundiLead}, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+            },
+            status: 200,
+        });
+    } catch(error){
+        return Response.json({error: "SOMETHING WENT WRONG"}, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+            },
+            status: 500,
+        });
+    }
 }
