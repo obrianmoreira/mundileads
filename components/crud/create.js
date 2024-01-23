c1425f80e3995e5a94c649bf6de4a487f2b0b7ee
obrianmoreira <110128226+obrianmoreira@users.noreply.github.com>
2024-01-23 00:26:48 +0000

    Update create.js

--- components/crud/create.js
+++ components/crud/create.js
@@ -5,12 +5,10 @@ export async function CreateMundiLead(name, userName, phone, birth) {
     try {
         const docRef = collection(database, 'mundi-leads');
         await setDoc(doc(docRef), {
-            message: {
-                name : name,
+            name : name,
                 userName: userName,
                 phone: phone,
                 birth: birth,
-            }
         })
     } catch(error) {
         console.log('errou, hein!')
